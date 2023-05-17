2818ebffe52af39499098566b6f613c4473e8de0
tallesjr <118635024+tallesjr@users.noreply.github.com>
2023-05-17 20:53:55 -0300

    atualização

---
+++ src/main/java/br/senac/sp/pi/DAO/TelaVendaProdutoDAO.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.senac.sp.pi.DAO;
+
+import br.senac.sp.pi.model.Clientes;
+
+/**
+ *
+ * @author Talles
+ */
+public class TelaVendaProdutoDAO{
+    
+    public static boolean pesquisa_cliente(Clientes cliente){
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ src/main/java/br/senac/sp/pi/Relatório.java
@@ -0,0 +1,12 @@
+package br.senac.sp.pi;
+
+import br.senac.sp.pi.view.TelaRelatorio;
+
+
+public class Relatório {
+
+    public static void main(String[] args) {
+        new TelaRelatorio().setVisible(true);
+    }
+    
+}
